989c42165cf4f66e8b14e8440a2eefba97e1df55
MilkMan127 <4394348@myuwc.ac.za>
2026-03-16 12:27:50 +0000

    Implement initial MyHeapSort class with swap method

--- tryHeapsort.java
+++ tryHeapsort.java
@@ -1,1 +1,9 @@
+import java.util.*;
 
+public class MyHeapSort {
+
+    public static void swap(String[] arr, int i, int j) {
+        String temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
