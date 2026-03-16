3b414595ebdd2e35970d3407576975ad1b1d7e46
MilkMan127 <4394348@myuwc.ac.za>
2026-03-16 12:29:59 +0000

    Add heapify method to MyHeapSort class for heap structure maintenance

--- tryHeapsort.java
+++ tryHeapsort.java
@@ -7,3 +7,20 @@ public class MyHeapSort {
         arr[i] = arr[j];
         arr[j] = temp;
     }
+    public static void heapify(String[] arr, int n, int i) {
+
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && arr[left].compareTo(arr[largest]) > 0)
+            largest = left;
+
+        if (right < n && arr[right].compareTo(arr[largest]) > 0)
+            largest = right;
+
+        if (largest != i) {
+            swap(arr, i, largest);
+            heapify(arr, n, largest);
+        }
+    }
